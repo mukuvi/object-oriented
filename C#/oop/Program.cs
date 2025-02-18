7f9c578953d6977a1d1364ed5be28c0bec2b670b
mukuvi <jamesmngandu@gmail.com>
2025-02-18 11:46:22 +0300

    OOP

--- C#/oop/Program.cs
+++ C#/oop/Program.cs
@@ -1,6 +1,16 @@
 ﻿using System;
-class myOop{
-    static void Main(){
-        Console.WriteLine("yoh");
-    }
+/*
+>Object-Oriented Programming
+>a class is a template for objects, and an object is an instance of a class
+*/
+class Car
+{
+  string brand = "Toyota";
+  static void Main(string[] args)
+  {
+    Car myObj1 = new Car();
+    Car myObj2 = new Car();
+    Console.WriteLine(myObj1.brand);
+  
+  }
 }
