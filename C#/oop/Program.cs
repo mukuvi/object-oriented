e15ea93f4737d7c3331d81cbefb4db7ffbf19d50
mukuvi <jamesmngandu@gmail.com>
2025-02-18 11:36:13 +0300

    OOP

--- C#/oop/Program.cs
+++ C#/oop/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class myOop{
+    static void Main(){
+        Console.WriteLine("yoh");
+    }
+}
