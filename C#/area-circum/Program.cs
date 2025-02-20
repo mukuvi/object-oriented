514a93e14a10f14d2ca31346bedb8b83f6ad6550
mukuvi <jamesmngandu@gmail.com>
2025-02-20 21:12:10 +0300

    OOP

---
+++ C#/area-circum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Circle
+{
+        private double radius;
+
+    public Circle(double r)
+    {
+        radius = r;
+    }
+
+    public double ComputeArea()
+    {
+        return Math.PI * radius * radius;
+    }
+
+    public double ComputeCircumference()
+    {
+        return 2 * Math.PI * radius;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Input radius
+        Console.Write("Enter the radius of the circle: ");
+        double radius = double.Parse(Console.ReadLine());
+
+        // Create Circle object
+        Circle circle = new Circle(radius);
+
+        Console.WriteLine($"Area of the circle: {circle.ComputeArea():F2}");
+        Console.WriteLine($"Circumference of the circle: {circle.ComputeCircumference():F2}");
+    }
+}
---
+++ C#/area-perimeter/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Rectangle
+{
+  
+    private double length;
+    private double width;
+
+    public Rectangle(double l, double w)
+    {
+        length = l;
+        width = w;
+    }
+
+    public double ComputeArea()
+    {
+        return length * width;
+    }
+
+    // Method to compute perimeter
+    public double ComputePerimeter()
+    {
+        return 2 * (length + width);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Input length and width
+        Console.Write("Enter the length of the rectangle: ");
+        double length = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter the width of the rectangle: ");
+        double width = double.Parse(Console.ReadLine());
+
+        // Create Rectangle object
+        Rectangle rectangle = new Rectangle(length, width);
+
+        Console.WriteLine($"Area of the rectangle: {rectangle.ComputeArea():F2}");
+        Console.WriteLine($"Perimeter of the rectangle: {rectangle.ComputePerimeter():F2}");
+    }
+}
