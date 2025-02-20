dd8869a2ace9fff44d5fe586fe652367c1de09d9
mukuvi <jamesmngandu@gmail.com>
2025-02-20 19:03:11 +0300

    task

---
+++ C#/tasks/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] marks = new int[100];
+        int sum = 0;
+
+        // Input marks for 100 students
+        for (int i = 0; i < 100; i++)
+        {
+            Console.Write($"Enter mark for student {i + 1}: ");
+            marks[i] = int.Parse(Console.ReadLine());
+            sum += marks[i];
+        }
+
+        // Compute mean
+        double mean = (double)sum / 100;
+        Console.WriteLine($"The mean mark of the class is: {mean}");
+    }
+}
