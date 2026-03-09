4e54479cf07de26ad53bfc08d160e436ec305d89
mukuvi <jamesmngandu@gmail.com>
2026-03-09 18:19:40 +0300

    OOP

--- Java/Script/Main.java
+++ Java/Script/Main.java
@@ -1,15 +1,16 @@
 /* JAVA*/
 public class Main{
     public static void main(String[] args){
-        System.out.println("Hakuna Matata");//hakuna matata
-         System.out.println(8+7);
          String name = "James";
          int age = 22;
          boolean student = true;
          char score = 'A';
          float number = 5.99f;
-         System.out.println(name + age);
-       
+         final int myNum = 7; // final is a java constant
+        //  System.out.println("My name is " +name+ " and my age is " +age);
+        int x, y,z;
+        x=y=z=40;
+      
         
     }
 }
