02b52b121e7a0994faaf37194246d56f7f31b98d
mukuvi <jamesmngandu@gmail.com>
2026-03-09 18:24:07 +0300

    OOP

--- Java/Script/Main.java
+++ Java/Script/Main.java
@@ -1,15 +1,18 @@
 /* JAVA*/
 public class Main{
     public static void main(String[] args){
-         String name = "James";
-         int age = 22;
-         boolean student = true;
-         char score = 'A';
-         float number = 5.99f;
+         String name = "James";//String
+         int age = 22; //Integer
+         boolean student = true; //Boolean
+         char score = 'A'; //Character
+         float number = 5.99f; //Floating point nummber
          final int myNum = 7; // final is a java constant
         //  System.out.println("My name is " +name+ " and my age is " +age);
-        int x, y,z;
-        x=y=z=40;
+        int l, w, area;
+        l = 4; w = 5; area = l * w;
+        System.out.println(area);
+
+
       
         
     }
