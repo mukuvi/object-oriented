ff8dd545c8e4a6cbd54e26b62cebc5eea1378b95
mukuvi <jamesmngandu@gmail.com>
2026-03-09 18:07:56 +0300

    OOP

--- Java/Script/Main.java
+++ Java/Script/Main.java
@@ -1,6 +1,7 @@
+/* JAVA*/
 public class Main{
     public static void main(String[] args){
-        System.out.println("Hakuna Matata");
+        System.out.println("Hakuna Matata");//hakuna matata
          System.out.println(8+7);
        
         
