8b6bc1070f348e9e83976bd7e7ce993b03d1bb4c
mukuvi <jamesmngandu@gmail.com>
2026-04-17 06:27:24 +0300

    OOP

---
+++ Java/test/Main.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello Mr");
+        // int num = 3;
+        // if(num<5){
+        //   System.out.println("Number less than 5");  
+        // }else{
+        //     System.out.println("Number greater than 5"); 
+        // }
+        for (int i =0; i<=5;i++){
+            System.out.println("I is: " +i);
+        }
+    }
+}
