d1f0a342eddb012f4ef75a5734aa5ea9919fbaed
mukuvi <jamesmngandu@gmail.com>
2026-04-17 06:36:41 +0300

    OOP

---
+++ Java/test/Whilo.java
@@ -0,0 +1,9 @@
+public class Whilo{
+    public static void main(String[] args){
+        int i = 0;
+        while(i<5){
+            System.out.println("I is : "+ i);
+            i++;
+        }
+    }
+}
